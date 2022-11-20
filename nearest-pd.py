2a6bc578852aeda0e5b0ad2190b58240be6ab102
Mohammad Namakshenas <51627278+namakshenas@users.noreply.github.com>
2022-11-20 16:44:39 +0330

    Add files via upload

---
+++ nearest-pd.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+def nearestPD(A):
+    """Find the nearest positive-definite matrix to input
+    N.J. Higham, "Computing a nearest symmetric positive semidefinite
+    matrix" (1988): https://doi.org/10.1016/0024-3795(88)90223-6
+    """
+    B = (A + A.T) / 2
+    _, s, V = np.linalg.svd(B)
+    H = np.dot(V.T, np.dot(np.diag(s), V))
+    A2 = (B + H) / 2
+    A3 = (A2 + A2.T) / 2
+    if isPD(A3):
+        return A3
+
+    spacing = np.spacing(np.linalg.norm(A))
+    
+    I = np.eye(A.shape[0])
+    k = 1
+    while not isPD(A3):
+        mineig = np.min(np.real(np.linalg.eigvals(A3)))
+        A3 += I * (-mineig * k**2 + spacing)
+        k += 1
+    return A3
+
+
+def isPD(B):
+    """Returns true when input is positive-definite, via Cholesky"""
+    try:
+        _ = np.linalg.cholesky(B)
+        return True
+    except np.linalg.LinAlgError:
+        return False
+
+
+if __name__ == '__main__':
+    import numpy as np
+    for i in range(10):
+        for j in range(2, 100):
+            A = np.random.randn(j, j)
+            B = nearestPD(A)
+            assert (isPD(B))
+    print('unit test passed!')
